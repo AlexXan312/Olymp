928c81c5eb381cbc34af282d500c00b40f87f3c8
alexanderSushin <62149707+alexanderSushin@users.noreply.github.com>
2022-09-15 10:17:48 +0300

    Update Checker.sh

--- Checker.sh
+++ Checker.sh
@@ -1,13 +1,16 @@
 g++ -std=c++20 main.cpp -o original
 g++ -std=c++20 stupid.cpp -o stupid
 g++ -std=c++20 gen.cpp -o gen
+declare -i var
 while true;
 do
     ./gen > input.txt
     ./stupid < input.txt > stupid_output.txt
     ./original < input.txt > original_output.txt
+    var=var+1
     if diff original_output.txt stupid_output.txt > difference.txt;
-    then echo "OK"
+    then
+      echo "TEST:" $var "OK"
     else
         echo "WA"
         echo "TEST:"
